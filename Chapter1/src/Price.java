90bd2a82acb642be4803460d89dbe38ae804aa92
admin <admin@DESKTOP-861H972>
2019-10-17 10:51:21 +0530

    Chpater 1

---
+++ Chapter1/src/Price.java
@@ -0,0 +1,21 @@
+/*
+ 16. Given the following class, which of the following lines of code can replace INSERT CODE
+HERE to make the code compile? (Choose all that apply)
+public class Price {
+public void admission() {
+INSERT CODE HERE
+System.out.println(amount);
+} }
+A. int amount = 9L;
+B. int amount = 0b101;
+C. int amount = 0xE;
+D. double amount = 0xE;
+E. double amount = 1_2_.0_0;
+F. int amount = 1_2_;
+G. None of the above.
+*/
+public class Price {
+public void admission() {
+	int amount = 1_2;
+System.out.println(amount);
+} }
