58085e8211888dd593b6546a75706e3781276b71
DELL <DELL@DESKTOP-KIVCQMG>
2019-10-18 10:08:27 +0530

    Chapter 2 question2

---
+++ Chapter2/src/Question2.java
@@ -0,0 +1,11 @@
+/* Which of the following compile? (Choose all that apply)
+A. final static void method4() { }
+B. public final int void method() { }
+C. private void int method() { }
+D. static final void method3() { }
+E. void final method() {}
+F. void public method() { }
+*/
+public class Question2 {
+
+}
