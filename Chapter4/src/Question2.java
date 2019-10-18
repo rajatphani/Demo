0195257dd10fed29a42eba2f5582664413292e58
DELL <DELL@DESKTOP-KIVCQMG>
2019-10-18 16:13:30 +0530

    chapter4 Question2

--- Chapter4/src/Question2.java
+++ Chapter4/src/Question2.java
@@ -1,4 +1,15 @@
-
+/*
+ * 10. What is the output of the following code? 
+1: package rope;
+2: public class Rope {
+3: public static int LENGTH = 5;
+4: static { 
+5: LENGTH = 10;
+6: }Review Qu7: public static void swing() {
+8: System.out.print("swing ");
+9: }
+10: }
+ */
 public class Question2 {
 
 }
