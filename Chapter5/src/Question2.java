8e6c3330ac4f71f48188d20376dcb1c6aec5097a
DELL <DELL@DESKTOP-KIVCQMG>
2019-10-18 16:37:34 +0530

    CHAPTER5 QUESTION2

--- Chapter5/src/Question2.java
+++ Chapter5/src/Question2.java
@@ -1,4 +1,24 @@
-
+/*
+ * . What is the output of the following code?
+1: class Arthropod 
+2: public void printName(double input) { System.out
+  .print("Arthropod"); }
+3: }
+4: public class Spider extends Arthropod {
+5: public void printName(int input) { System.out.print("Spider"); 
+6: public static void main(String[] args) {
+7: Spider spider = new Spider();
+8: spider.printName(4);
+9: spider.printName(9.0);
+10: }
+11: }
+A. SpiderArthropod
+B. ArthropodSpider
+C. SpiderSpider
+D. ArthropodArthropod
+E. The code will not compile because of line 5.
+F. The code will not compile because of line 9.
+ */
 public class Question2 {
 
 }
