30330d7c5cd0502bae26dd35dc989bffba3d5337
DELL <DELL@DESKTOP-KIVCQMG>
2019-10-18 16:41:40 +0530

    CHAPTER5 QUESTION3

---
+++ Chapter5/src/QUESTION3.java
@@ -0,0 +1,19 @@
+/*
+ * . What is the output of the following code?
+1: public abstract class Whale {
+2: public abstract void dive() {};
+3: public static void main(String[] args) {
+4: Whale whale = new Orca();
+5: whale.dive();
+6: }
+7: }Rev8: class Orca extends Whale {
+9: public void dive(int depth) { System.out.println(10: }
+A. Orca diving
+B. The code will not compile because of line 2.
+C. The code will not compile because of line 8.
+D. The code will not compile because of line 9.
+E. The output cannot be determined from the code provided.
+ */
+public class QUESTION3 {
+
+}
