f2896ce1a20e8944f64918bb5b208e7118a58b79
DELL <DELL@DESKTOP-KIVCQMG>
2019-10-18 16:30:56 +0530

    CHAPTER5 QUESTION1

---
+++ Chapter5/src/Question1.java
@@ -0,0 +1,22 @@
+/*
+ * 2. What is the output of the following code?
+1: class Mammal {
+2: public Mammal(int age) {
+3: System.out.print("Mammal");
+4: }
+5: }
+6: public class Platypus extends Mammal 7: public Platypus() {
+8: System.out.print("Platypus");
+9: }
+10: public static void main(String[] 11: new Mammal(5);
+12: }
+13: }
+A. Platypus
+B. Mammal
+C. PlatypusMammal
+D. MammalPlatypus
+E. The code will not compile because of line F. The code will not compile because of line
+ */
+public class Question1 {
+
+}
