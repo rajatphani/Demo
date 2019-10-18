e4685fe8f5474a78e1efb13ed6108524e1ceadd4
DELL <DELL@DESKTOP-KIVCQMG>
2019-10-18 16:08:09 +0530

    Chapter4 Question1

---
+++ Chapter4/src/Question1.java
@@ -0,0 +1,14 @@
+/*
+ *  Given the following my.school.ClassRoom and my.city.School class definitions, whicline numbers in main() generate a compiler error? (Choose all that apply)
+1: package my.school;
+2: public class Classroom {
+3: private int roomNumber;
+4: protected String teacherName;
+5: static int globalKey = 54321;
+6: public int floor = 3;
+7: Classroom(int r, String t) {
+8: roomNumber = r;
+ */
+public class Question1 {
+
+}
